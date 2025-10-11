2550c12013be76462bddfccc39ee6186f4d59f41
pinarcelik <220290060@firat.edu.tr>
2025-10-11 14:01:49 +0300

    foreach eklendi

--- 01-variable-data-types/01-console/script.js
+++ 01-variable-data-types/01-console/script.js
@@ -1,14 +1,14 @@
 console.log('Hello World');
 console.log(100);
-console.log('hello',100,true);
+console.log('hello', 100, true);
 
-const x=100;
+const x = 100;
 console.log(x);
 
 console.error('Alert');
 console.warn('Warning');
 
-console.table({name:'Brad', email:'brad@gmail.com'});
+console.table({ name: 'Brad', email: 'brad@gmail.com' });
 console.group('simple');
 
 console.log(x);
@@ -16,15 +16,17 @@ console.error('Alert');
 console.groupEnd();
 console.warn('Warning');
 
-const styles='padding:10px; background-color:yellow; color:';
+const styles = 'padding:10px; background-color:yellow; color:';
 console.log('%cHello World', styles);
 
-const arr=[1,2,3,4,5];
+const arr = [1, 2, 3, 4, 5];
 arr.push(6);
 console.log(arr);
 
-let a,b,c;
-const d=10;
-e=20;
-f=30;
-console.log(d,e,f);
+let a, b, c;
+const d = 10;
+e = 20;
+f = 30;
+console.log(d, e, f);
+
+console.log(window)
--- 01-variable-data-types/boy-kilo indexi/app.js
+++ 01-variable-data-types/boy-kilo indexi/app.js
@@ -1,20 +1,27 @@
-let kilo=Number(prompt("Kilonuzu giriniz:"));
+let kilo = Number(prompt("Kilonuzu giriniz:"));
 
-let boy=Number(prompt("Boyunuzu giriniz:"));
+let boy = Number(prompt("Boyunuzu giriniz:"));
 
-let sonuc=kilo/(boy*2);
+let sonuc = kilo / (boy * 2);
 
-if(sonuc<18.6){
-    console.log("ideal kilonun altındasınız" +sonuc);
-}else if(sonuc>=18.6 && sonuc<24.9){
-    console.log("ideal kilodasınız" +sonuc);
-}else if(sonuc>=24.9 && sonuc<29.9){
-    console.log("ideal kilonun üstündesiniz" +sonuc);
+if (sonuc < 18.6) {
+    console.log("ideal kilonun altındasınız" + sonuc);
+} else if (sonuc >= 18.6 && sonuc < 24.9) {
+    console.log("ideal kilodasınız" + sonuc);
+} else if (sonuc >= 24.9 && sonuc < 29.9) {
+    console.log("ideal kilonun üstündesiniz" + sonuc);
 }
-else if(sonuc>=29.9 && sonuc<39.9){
-    console.log("ideal kilonun çok üstündesiniz(obez)" +sonuc);
+else if (sonuc >= 29.9 && sonuc < 39.9) {
+    console.log("ideal kilonun çok üstündesiniz(obez)" + sonuc);
 }
-else if(sonuc>=39.9){
-    console.log("ideal kilonun çok üstündesiniz(morbid obez)"+sonuc);
+else if (sonuc >= 39.9) {
+    console.log("ideal kilonun çok üstündesiniz(morbid obez)" + sonuc);
 }
+
+//Foreach Döngüsüne örnek
+let names = ["ali", "veli", "can"];
+names.forEach(function (name) {
+
+    console.log(name)
+});
 
