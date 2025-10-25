c43ed262dd662a8ba49bf1b4074faca3287f7437
pinarcelik <220290060@firat.edu.tr>
2025-10-25 16:44:29 +0300

    Elemen silme eklendi

---
+++ 01-variable-data-types/48.Element Silme/app.js
@@ -0,0 +1,11 @@
+//Element Silme
+
+const todoList = document.querySelector(".list-group");
+const todos = document.querySelectorAll(".list-group-item");
+
+todos[0].remove();
+
+//bunu ben elimle yazdımm ve çalıştıııı!!!!
+console.log(todos[1].remove());
+
+console.log(todoList);
