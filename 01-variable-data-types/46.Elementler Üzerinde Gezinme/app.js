30e0ce4dbfd0c7e9f41e9f1d3ec514f51c997d91
pinarcelik <220290060@firat.edu.tr>
2025-10-25 16:19:57 +0300

    element oluştuma eklendi

---
+++ 01-variable-data-types/46.Elementler Üzerinde Gezinme/app.js
@@ -0,0 +1,61 @@
+// HTML Elementleri Üzerinde Gezinmek.
+
+const todo = document.querySelector(".list-group-item");
+const todoList = document.querySelector(".list-group");
+const todoLastChild = document.querySelector(".list-group-item:nth-child(4)");
+// const card = document.getElementsByClassName("card")[0];
+const card = document.querySelector(".card");
+const contain = document.querySelector(".container");
+
+let value;
+
+//Anneden çocuklara erişmek
+
+value = todoList;
+value = todoList.children[0];
+value = todoList.children[3];
+value = todoList.children[todoList.children.length - 1];
+value = todoList.children[3].textContent = "Değişti";
+
+// value = Array.from(todoList.children);
+
+// value.forEach(function(todo){
+//     console.log(todo.textContent);
+// })
+
+
+
+//Çocuktan Anneye Erişmek
+
+value = todo;
+let ul = todo.parentElement;
+let cardBody = ul.parentElement;
+let cardElement = cardBody.parentElement;
+let row = cardElement.parentElement;
+let container = row.parentElement;
+
+
+// Kardeşler arasında gezinmek
+value = todo;
+value = todo.nextElementSibling.nextElementSibling.nextElementSibling;
+
+
+value = todoLastChild;
+value = todoLastChild.previousElementSibling.previousElementSibling
+    .previousElementSibling.nextElementSibling.previousElementSibling
+    .previousElementSibling;
+
+
+value = row.children[0].children[3].children[0].textContent = "Todo Listesi Başlığı Değişmedi";
+
+let todo3 = row.children[0].children[3].children[2].children[2];
+todo3.textContent = "Todo3 Değişti";
+todo3.style.backgroundColor = "lightgrey";
+todo3.style.color = "red";
+
+value = contain;
+
+let todo2 = contain.children[0].children[0].children[1].children[0].children[1]; todo2.textContent = "Todo ekleyin lütfen hemen";
+todo2.style.backgroundColor = "green";
+todo2.style.color = "white";
+console.log(todo2);
