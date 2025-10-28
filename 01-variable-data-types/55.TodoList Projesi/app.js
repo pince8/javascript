2b52c997534f2a9862e36ef706a490c8f14f9ce3
pinarcelik <pcelik157@gmail.com>
2025-10-28 23:15:51 +0300

    todoList projesine başlandı, arayüze ve stoargea kaydedildi todolar

---
+++ 01-variable-data-types/55.TodoList Projesi/app.js
@@ -0,0 +1,71 @@
+//Tüm Elementleri Seçmek
+
+const form = document.querySelector("#todoAddForm");
+const addInput = document.querySelector("#todoName");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const clearButton = document.querySelector("#clearButton");
+
+let todos = [];
+
+runEvents();
+
+function runEvents() {
+    form.addEventListener("submit", addTodo);
+}
+
+function addTodo(e) {
+    const inputText = addInput.value.trim();
+    if (inputText == null || inputText == "") {
+        alert("Lütfen bir değer giirniz!");
+    } else {
+        //Arayüz ekleme
+        addTodoToUI(inputText);
+        //storage ekleme
+        addTodoToStorage(inputText);
+    }
+
+    e.preventDefault();
+}
+
+function addTodoToUI(newTodo) {
+    /*
+                        <li class="list-group-item d-flex justify-content-between">Todo 1
+                            <a href="#" class="delete-item">
+                                <i class="fa fa-remove"></i>
+                            </a>
+                        </li>
+                    */
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between";
+    li.textContent = newTodo;
+
+    const a = document.createElement("a");
+    a.href = "#";
+    a.className = "delete-item";
+
+    const i = document.createElement("i");
+    i.className = "fa fa-remove";
+
+    a.appendChild(i);
+    li.appendChild(a);
+    todoList.appendChild(li);
+
+    addInput.value = "";
+
+}
+
+function addTodoToStorage(newTodo) {
+    checkTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function checkTodosFromStorage() {
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+}
